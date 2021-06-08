677b4a62180301acc91c44395bd15096c0959dff
Dhriti-31 <78593550+Dhriti-31@users.noreply.github.com>
2021-06-08 06:02:48 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -7,6 +7,8 @@ class Block{
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
+      this.image = loadImage("block.png");
+      this.visibility = 255;
       World.add(world, this.body);
     }
    // display(){
@@ -20,13 +22,13 @@ class Block{
        var pos= this.body.position;
           rectMode(CENTER);
           rect(pos.x,pos.y,this.width, this.height);
-      } else{
+       } else{
         World.remove(world,this.body);
         push();
         this.visibility = this.visibility-5;
-        tint(255,this.visibility);
-        image(this.image, this.body.position.x,this.body.position.y,50,50);
+        tint(225,this.visibility);
+        image(this.image, this.body.position.x,this.body.position.y,30,40);
         pop();
-      }
+          }  
     }
 }
